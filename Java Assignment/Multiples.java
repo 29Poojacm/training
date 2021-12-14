02507ed8fc127e7e39a16a24b37dda5fffe84279
29Poojacm <89911081+29Poojacm@users.noreply.github.com>
2021-12-14 14:39:39 +0530

    Add files via upload

---
+++ Java Assignment/Multiples.java
@@ -0,0 +1,14 @@
+
+ public class Multiples{
+    public static void main (String [] args){
+        int n=2;
+        
+        System.out.println ("Enter the numbers");
+         
+
+        for (int i=1; i<=10; i++)
+        {
+            System.out.println (n+" * "+i+" = "+n*i);
+        }
+    }
+}
